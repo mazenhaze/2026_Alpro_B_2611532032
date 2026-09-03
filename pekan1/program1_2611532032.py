6f6b97411295376a4be469c02a183b6c33db75f6
mazenhaze <rauff5258@gmail.com>
2026-09-03 10:21:26 +0700

    Add files via upload

---
+++ pekan1/program1_2611532032.py
@@ -0,0 +1,12 @@
+# program pertama
+"""
+komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print ("hello world")
+print ()
+print ("program ini menghasilkan")
+print ("empat baris output")
+print ("/ \\ // \\\\ /// \\\\\\")
+print ("program ini" + " dibuat oleh a\n" + "M Abdurrauf_2611532032")
